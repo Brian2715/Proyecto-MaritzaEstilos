621043d92231fe6cbc3b2693870c15c1b4f6b049
Brian2715 <bans.nkd.15@gmail.com>
2023-09-11 23:54:47 -0300

    actualizacion del sprint 2

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const path = require('path');
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.listen(5000, ()=>{
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
     console.log('Servidor funcionando');
 });
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/src/views/home.html');
+});
+
+app.get('/contacto.html', (req, res) =>{
+    res.sendFile(__dirname + '/src/views/contacto.html');
 });
